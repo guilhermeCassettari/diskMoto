7fd057d7eed5dec459bfc050a343b5c1d9b44879
Guilherme Gasparotti Cassettari <guizaocassettari@gmail.com>
2021-04-12 17:09:07 -0300

    colocando o .next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "90ea088749049d20353f";
+/******/ 	var hotCurrentHash = "0817186f36e2bff59242";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
